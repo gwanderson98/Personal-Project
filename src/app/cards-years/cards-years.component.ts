a5484da30d3331ec2bf813613fbe1dcbaf4c9ce4
Wanderson Moura Oliveira <wmouraol@everis.com>
2019-10-22 17:31:02 -0300

    Init config cards

--- src/app/cards-years/cards-years.component.ts
+++ src/app/cards-years/cards-years.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { $ } from 'protractor';
 
 @Component({
   selector: 'app-cards-years',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardsYearsComponent implements OnInit {
 
+  flipped = false;
+  flipped1 = false;
+  flipped2 = false;
+  indexFlipped = 0;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  flipIt(indexFlipped) {
+    if(indexFlipped == 1){
+      this.flipped = !this.flipped;
+    }else if(indexFlipped == 2){
+      this.flipped1 = !this.flipped1;
+    }else if(indexFlipped == 3){
+      this.flipped2 = !this.flipped2;
+    }
+  }
 }
--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -15,9 +15,6 @@ export class IntroComponent implements OnInit {
   
     banners: Banner[] = [
       imgJapa1, imgJapa2, imgTchiquen
-      // { id: 1, name:'japa1', path: imgJapa1 },
-      // { id: 2,  name:'japa2', path: imgJapa2 },
-      // { id: 3,  name:'tchiquen', path: imgTchiquen }
     ];
     
 
